6f32ec80f59fd98acd5d80ff542fa991f3780e20
jrdx86 <jrdx86@gmail.com>
2017-10-03 09:30:50 +0200

    Añadido HolaMundo con eclipse

---
+++ HolaMundo/src/org/institutoserpis/ed/JHolaMundo.java
@@ -0,0 +1,18 @@
+package org.institutoserpis.ed;
+
+import java.util.Scanner;
+
+
+
+public class JHolaMundo {
+	public static void main(String[] args) {
+		System.out.print("Dime un numero: ");
+		int primero = Integer.parseInt(primerNumero);
+		Scanner scanner = new Scanner (System.in);
+		System.out.print("Dime otro: ");
+		int segundo = Integer.parseInt(segundoNumero);
+		String nombre = scanner.nextLine();
+		System.out.println("Hoooola " + nombre);
+	}
+
+}
